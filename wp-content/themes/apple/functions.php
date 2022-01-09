f2e49b8c45b613419c1e231792ecf39785861db6
VasilenkoRuslan <vasilenko.ruslan0321@gmail.com>
2022-01-09 23:34:46 +0200

    ADDED | WP theme

---
+++ wp-content/themes/apple/functions.php
@@ -0,0 +1,7 @@
+<?php
+define('THEME_DIR', get_template_directory());
+define('THEME_DIR_URI', get_template_directory_uri());
+
+require_once THEME_DIR .'/inc/helpers/file.php';
+require_once THEME_DIR .'/inc/wp/enqueue_scripts.php';
+require_once THEME_DIR .'/inc/wp/theme_support.php';
