019ad7d87522aee5c13bf9541f759a962003cf9b
VasilenkoRuslan <vasilenko.ruslan0321@gmail.com>
2022-01-10 21:57:55 +0200

    UPDATED | small fixes

--- wp-content/themes/apple/functions.php
+++ wp-content/themes/apple/functions.php
@@ -2,6 +2,9 @@
 define('THEME_DIR', get_template_directory());
 define('THEME_DIR_URI', get_template_directory_uri());
 
+/* link to html plate */
+$temp_site_dir = THEME_DIR_URI . '/junno/'; //Todo: delete before production
+
 require_once THEME_DIR .'/inc/helpers/file.php';
 require_once THEME_DIR .'/inc/wp/enqueue_scripts.php';
 require_once THEME_DIR .'/inc/wp/theme_support.php';
--- wp-content/themes/apple/inc/wp/theme_support.php
+++ wp-content/themes/apple/inc/wp/theme_support.php
@@ -4,3 +4,4 @@
  */
 
 add_theme_support('post-thumbnails');
+add_theme_support('title-tag');
