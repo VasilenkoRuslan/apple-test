5081383c8fd360da1863c0cff8ff5b320ca30153
VasilenkoRuslan <vasilenko.ruslan0321@gmail.com>
2022-02-08 01:42:23 +0200

    FIX | display widget and searchform

---
+++ wp-content/themes/apple/assets/scripts/helpers/add_searchform_class.js
@@ -0,0 +1,5 @@
+/* eslint-env jquery */
+jQuery( document ).ready(function() {
+  jQuery(".sidebar-widget .search-form .form-inline .form-control").addClass("rounded");
+  jQuery(".sidebar-widget .search-form .form-inline .btn").addClass("blog-search-btn");
+});
---
+++ wp-content/themes/apple/assets/scripts/helpers/add_widget_ul_class.js
@@ -0,0 +1,5 @@
+/* eslint-env jquery */
+jQuery( document ).ready(function() {
+  jQuery(".footer-widget .menu-information-footer-container > ul, .footer-widget .menu-custom-links-footer-container > ul").addClass("footer-menu");
+  jQuery(".mega-menu li > ul").addClass("mega-menu");
+});
