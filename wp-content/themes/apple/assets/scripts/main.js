ed68718f8e55d1a2274ab602a5d6780610ccab54
VasilenkoRuslan <vasilenko.ruslan0321@gmail.com>
2022-02-08 02:11:09 +0200

    ADDED | main files(sass, js)

--- wp-content/themes/apple/assets/scripts/main.js
+++ wp-content/themes/apple/assets/scripts/main.js
@@ -1,1 +1,18 @@
-//import 'file'
+// node modules scripts
+import "popper.js/dist/popper.min"
+import "jquery-ui-bundle.1.12.1/jquery-ui.min"
+import "bootstrap/dist/js/bootstrap.min"
+import "jquery-countdown/dist/jquery.countdown.min"
+import "slick-animation/slick-animation.min"
+import "jquery.scrollup/src/jquery.scrollUp"
+
+// custom scripts
+import "./plugins/plugins"
+import "./theme_js"
+
+// helpers
+import "./helpers/add_widget_ul_class"
+import "./helpers/add_searchform_class"
+
+// pages
+import "./pages/news"
